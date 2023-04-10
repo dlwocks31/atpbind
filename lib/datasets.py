da1fa83d6e2921f3bb26e9e2e7b19ab4a317c495
Jaechan <dlwocks31@gmail.com>
2023-04-10 11:59:33 +0900

    받은 sample code 추가

---
+++ lib/datasets.py
@@ -0,0 +1,96 @@
+import torch
+from torch.utils import data as torch_data
+from torchdrug import data
+
+
+class ATPBind(data.ProteinDataset):
+    splits = ["train", "valid", "test"]
+    target_fields = ['binding']
+    
+
+    def __init__(self, path, verbose=1, valid_ratio=0.1, **kwargs):
+        self.num_samples = []
+        self.valid_ratio=valid_ratio
+        sequences, targets = self.get_seq_target(path)
+        self.load_sequence(sequences, targets)
+        #[20,21,41]#[350, 38, 41]
+        
+
+    def get_seq_target(self, path):
+        sequences, targets = [], []
+
+        with open(path + 'train.txt') as f:
+            lines = f.readlines()
+            self.num_samples.append(len(lines))
+            for line in lines:
+                sequence = line.split(' : ')[-1].strip()
+                sequences.append(sequence)
+                
+                target = line.split(' : ')[-2].split(' ')
+                target_indices = []
+                for index in target:
+                    target_indices.append(int(index[1:]))
+                target = []
+                for index in range(len(sequence)):
+                    if index+1 in target_indices: 
+                        target.append(1)
+                    else:
+                        target.append(0)
+                targets.append(target)
+
+        with open(path + 'test.txt') as f:
+            lines = f.readlines()
+            self.num_samples.append(len(lines))
+            for line in lines:
+                sequence = line.split(' : ')[-1].strip()
+                sequences.append(sequence)
+                
+                target = line.split(' : ')[-2].split(' ')
+                target_indices = []
+                for index in target:
+                    target_indices.append(int(index[1:]))
+                target = []
+                for index in range(len(sequence)):
+                    if index+1 in target_indices: 
+                        target.append(1)
+                    else:
+                        target.append(0)
+                targets.append(target)
+
+        # calculate set lengths
+        total_samples = sum(self.num_samples)
+        val_num = int(total_samples*self.valid_ratio)
+        self.num_samples=[self.num_samples[0]-val_num,val_num,self.num_samples[1]]
+        print('Split num: ', self.num_samples)
+        
+        targets_ = {"binding": targets}
+        return sequences, targets_
+
+
+
+    def get_item(self, index):
+        if self.lazy:
+            graph = data.Protein.from_sequence(self.sequences[index], **self.kwargs)
+        else:
+            graph = self.data[index]
+        with graph.residue():
+            target = torch.as_tensor(self.targets["binding"][index], dtype=torch.long).view(-1, 1)
+            graph.target = target
+            mask = torch.ones_like(target).bool()
+            graph.mask = mask
+        graph.view = 'residue'
+        item = {"graph": graph}
+        if self.transform:
+            item = self.transform(item)
+        return item
+
+    def split(self, keys=None):
+        keys = keys or self.splits
+        offset = 0
+        splits = []
+        for split_name, num_sample in zip(self.splits, self.num_samples):
+            if split_name in keys:
+                split = torch_data.Subset(self, range(offset, offset + num_sample))
+                splits.append(split)
+            offset += num_sample
+        return splits
---
+++ lib/tasks.py
@@ -0,0 +1,129 @@
+import torch
+from torch.nn import functional as F
+from torchdrug import core, layers, metrics, tasks
+from torchdrug.layers import functional
+
+
+class NodePropertyPrediction(tasks.Task, core.Configurable):
+    ## Nov 4: MinGyu Choi
+    ### I found out that some problems emerge if we directly use NodePropertyPrediction.
+    ### I guess this is the problem of target setting, but correcting them seems to require a lot of time...
+    ### Therefore I just edited some tensor shapes for this specific case.
+    ### It might be cumbersome to do; but please understand.
+
+    _option_members = {"criterion", "metric"}
+
+    def __init__(self, model, criterion="bce", metric=("macro_auprc", "macro_auroc"), num_mlp_layer=1,
+                 normalization=True, num_class=None, verbose=0):
+        super(NodePropertyPrediction, self).__init__()
+        self.model = model
+        self.criterion = criterion
+        self.metric = metric
+        # For classification tasks, we disable normalization tricks.
+        self.normalization = normalization and ("ce" not in criterion) and ("bce" not in criterion)
+        self.num_mlp_layer = num_mlp_layer
+        self.num_class = num_class
+        self.verbose = verbose
+
+    def preprocess(self, train_set, valid_set, test_set):
+        """
+        Compute the mean and derivation on the training set.
+        """
+        self.view = getattr(train_set[0]["graph"], "view", "atom")
+        values = torch.cat([data["graph"].target for data in train_set])
+        mean = values.float().mean()
+        std = values.float().std()
+        if values.dtype == torch.long:
+            num_class = values.max().item()
+            if num_class > 1 or "bce" not in self.criterion:
+                num_class += 1
+        else:
+            num_class = 1
+
+        self.register_buffer("mean", torch.as_tensor(mean, dtype=torch.float))
+        self.register_buffer("std", torch.as_tensor(std, dtype=torch.float))
+        self.num_class = self.num_class or num_class
+
+        if hasattr(self.model, "node_output_dim"):
+            model_output_dim = self.model.node_output_dim
+        else:
+            model_output_dim = self.model.output_dim
+        hidden_dims = [model_output_dim] * (self.num_mlp_layer - 1)
+        self.mlp = layers.MLP(model_output_dim, hidden_dims + [self.num_class])
+
+    def predict(self, batch, all_loss=None, metric=None):
+        graph = batch["graph"]
+        output = self.model(graph, graph.node_feature.float(), all_loss=all_loss, metric=metric)
+        if self.view in ["node", "atom"]:
+            output_feature = output["node_feature"]
+        else:
+            output_feature = output.get("residue_feature", output.get("node_feature"))
+        pred = self.mlp(output_feature)
+        if self.normalization:
+            pred = pred * self.std + self.mean
+        return pred
+
+    def target(self, batch):
+        size = batch["graph"].num_nodes if self.view in ["node", "atom"] else batch["graph"].num_residues
+        return {
+            "label": batch["graph"].target,
+            "mask": batch["graph"].mask,
+            "size": size
+        }
+
+    def forward(self, batch):
+        """"""
+        all_loss = torch.tensor(0, dtype=torch.float32, device=self.device).view(1)
+        metric = {}
+
+        pred, target = self.predict_and_target(batch, all_loss, metric)
+        labeled = ~torch.isnan(target["label"]) & target["mask"]
+
+        for criterion, weight in self.criterion.items():
+            if criterion == "mse":
+                if self.normalization:
+                    loss = F.mse_loss((pred - self.mean) / self.std, (target - self.mean) / self.std, reduction="none")
+                else:
+                    loss = F.mse_loss(pred, target, reduction="none")
+            elif criterion == "bce":
+                loss = F.binary_cross_entropy_with_logits(pred, target["label"].float(), reduction="none")
+            elif criterion == "ce":
+                loss = F.cross_entropy(pred, target["label"], reduction="none")
+            else:
+                raise ValueError("Unknown criterion `%s`" % criterion)
+            loss = functional.masked_mean(loss, labeled, dim=0)
+
+            name = tasks._get_criterion_name(criterion)
+            metric[name] = loss
+            all_loss += loss * weight
+
+        all_loss += loss
+
+        return all_loss, metric
+
+    def evaluate(self, pred, target):
+        metric = {}
+        _target = target["label"]
+        _labeled = ~torch.isnan(_target) & target["mask"]
+        _size = functional.variadic_sum(_labeled.long(), target["size"]) .view(-1)
+        for _metric in self.metric:
+            if _metric == "micro_acc":
+                score = metrics.accuracy(pred[_labeled], _target[_labeled].long())
+            elif metric == "micro_auroc":
+                score = metrics.area_under_roc(pred[_labeled], _target[_labeled])
+            elif metric == "micro_auprc":
+                score = metrics.area_under_prc(pred[_labeled], _target[_labeled])
+            elif _metric == "macro_auroc":
+                score = metrics.variadic_area_under_roc(pred[_labeled], _target[_labeled], _size).mean()
+            elif _metric == "macro_auprc":
+                score = metrics.variadic_area_under_prc(pred[_labeled], _target[_labeled], _size).mean()
+            elif _metric == "macro_acc":
+                score = pred[_labeled].argmax(-1) == _target[_labeled]
+                score = functional.variadic_mean(score.float(), _size).mean()
+            else:
+                raise ValueError("Unknown criterion `%s`" % _metric)
+
+            name = tasks._get_metric_name(_metric)
+            metric[name] = score
+
+        return metric
