6bb908290ed34209d63eb527ecf1e16cc3463713
dlwocks31 <dlwocks31@gmail.com>
2023-04-19 13:56:26 +0900

    add generated pdb files

---
+++ data/script/generate_pdb.py
@@ -0,0 +1,155 @@
+from Bio.PDB import Select, PDBIO
+from Bio.PDB.PDBParser import PDBParser
+from torchdrug import utils
+import os
+import pylcs
+from torchdrug import data
+import warnings
+
+# Reference: https://stackoverflow.com/a/47584587/12134820
+
+
+class ChainSelect(Select):
+    def __init__(self, chain):
+        self.chain = chain
+
+    def accept_chain(self, chain):
+        if chain.get_id() == self.chain:
+            return 1
+        else:
+            return 0
+
+    def accept_residue(self, residue):
+        hetflag, _, _ = residue.get_id()
+        return hetflag == " "  # Should only accept normal residue (not HETATM)
+
+    def accept_atom(self, atom):
+        # Should only accept backbone atoms
+        return atom.get_name() in ["N", "CA", "C", "O"]
+
+
+class LCSSelect(Select):
+    def __init__(self, residue_seq):
+        self.residue_seq = set(residue_seq)
+
+    def accept_residue(self, residue):
+        _, resseq, _ = residue.get_id()
+        # Should only accept residue in longest substring
+        return resseq in self.residue_seq
+
+
+three_to_one_letter = {
+    'ALA': 'A', 'CYS': 'C', 'ASP': 'D', 'GLU': 'E',
+    'PHE': 'F', 'GLY': 'G', 'HIS': 'H', 'ILE': 'I',
+    'LYS': 'K', 'LEU': 'L', 'MET': 'M', 'ASN': 'N',
+    'PRO': 'P', 'GLN': 'Q', 'ARG': 'R', 'SER': 'S',
+    'THR': 'T', 'VAL': 'V', 'TRP': 'W', 'TYR': 'Y'
+}
+
+
+def read_file(path):
+    '''
+    Read from ATPBind dataset.
+    '''
+    sequences, targets, pdb_ids = [], [], []
+    with open(path) as f:
+        lines = f.readlines()
+        num_samples = len(lines)
+        for line in lines:
+            sequence = line.split(' : ')[-1].strip()
+            sequences.append(sequence)
+
+            target = line.split(' : ')[-2].split(' ')
+            target_indices = []
+            for index in target:
+                target_indices.append(int(index[1:]))
+            target = []
+            for index in range(len(sequence)):
+                if index+1 in target_indices:
+                    target.append(1)
+                else:
+                    target.append(0)
+            targets.append(target)
+
+            pdb_id = line.split(' : ')[0]
+            pdb_ids.append(pdb_id)
+    return num_samples, sequences, targets, pdb_ids
+
+
+def write_parsed_pdb_from_pdb_id(atpbind_sequence, pdb_id):
+    if os.path.exists("./pdb_tmp/%s.pdb" % pdb_id[:4]):
+        file_path = "./pdb_tmp/%s.pdb" % pdb_id[:4]
+    else:
+        file_path = utils.download(
+            "https://files.rcsb.org/download/%s.pdb" % pdb_id[:4], "./pdb_tmp")
+
+    # First stage: filter for desired chain, normal residue, backbone atom
+    chain_id = pdb_id[4]
+    p = PDBParser(PERMISSIVE=1)
+    structure = p.get_structure(pdb_id[:4], file_path)
+    select = ChainSelect(chain_id)
+    pdbio = PDBIO()
+    pdbio.set_structure(structure)
+    pdbio.save('pdb_tmp/%s.pdb' % pdb_id, select)
+
+    # Second Stage: filter for longest substring
+    file_path = 'pdb_tmp/%s.pdb' % pdb_id
+    p = PDBParser(PERMISSIVE=1)
+    structure = p.get_structure(pdb_id, file_path)
+    raw_sequence = ''.join(three_to_one_letter[residue.get_resname()]
+                           for residue in structure.get_residues())
+    idx_to_use = pylcs.lcs_string_idx(atpbind_sequence, raw_sequence)
+    if -1 in idx_to_use:
+        print('warn: -1 in lss. Using lcs instead. %s' % pdb_id)
+        idx_to_use = pylcs.lcs_sequence_idx(atpbind_sequence, raw_sequence)
+        assert(-1 not in idx_to_use)
+    resseq_ids = [residue.get_id()[1] for i, residue in enumerate(
+        structure.get_residues()) if i in idx_to_use]
+    select = LCSSelect(resseq_ids)
+    pdbio = PDBIO()
+    pdbio.set_structure(structure)
+    pdbio.save('../pdb/%s.pdb' % pdb_id, select)
+
+
+def generate_all_in_file(filename):
+    _, sequences, _, pdb_ids = read_file(filename)
+    for sequence, pdb_id in zip(sequences, pdb_ids):
+        print('Generating %s..' % pdb_id)
+        write_parsed_pdb_from_pdb_id(sequence, pdb_id)
+
+
+def try_loading_pdb(file_path):
+    try:
+        protein = data.Protein.from_pdb(file_path)
+        return protein
+    except Exception as e:
+        print("Error loading %s" % file_path)
+        return None
+
+
+def validate(filename):
+    _, sequences, _, pdb_ids = read_file(filename)
+    for sequence, pdb_id in zip(sequences, pdb_ids):
+        print('Validating %s..' % pdb_id)
+        file_path = 'pdb/%s.pdb' % pdb_id
+        protein = try_loading_pdb(file_path)
+        if not protein:
+            continue
+        protein_sequence = ''.join(
+            i for i in protein.to_sequence() if i != '.')
+        if protein_sequence != sequence:
+            print('validation failed. len: %d %d' %
+                  (len(protein_sequence), len(sequence)))
+            print(protein_sequence)
+            continue
+
+
+if __name__ == '__main__':
+    warnings.filterwarnings("ignore", message=".*discontinuous at line.*")
+    warnings.filterwarnings("ignore", message=".*Unknown.*")
+    print('Generate train set..')
+    generate_all_in_file('../../lib/train.txt')
+    # 4 of the train set generated pdb cannot be loaded with data.Protein.from_pdb - check with `validate()`
+    print('Generate test set..')
+    generate_all_in_file('../../lib/test.txt')
+    # validate('../../lib/train.txt')
