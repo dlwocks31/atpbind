f70da21834dcb2e430ed23f537f116015d790c04
ihatedebug <jongh97@snu.ac.kr>
2023-05-17 16:13:49 +0900

    mcc threshold eval added to pipeline.py

--- lib/pipeline.py
+++ lib/pipeline.py
@@ -6,6 +6,7 @@ import torch
 from torch.utils import data as torch_data
 from torch.nn import functional as F
 import logging
+import numpy as np
 
 from .tasks import NodePropertyPrediction
 from .datasets import ATPBind
@@ -21,9 +22,11 @@ class DisableLogger():
 class Pipeline:
     possible_models = ['bert']
     possible_datasets = ['atpbind']
-    
+    threshold = 0
     
     def __init__(self, model, dataset, gpus, model_kwargs={}, task_kwargs={}):
+        self.gpus = gpus
+
         if model not in self.possible_models:
             raise ValueError('Model must be one of {}'.format(self.possible_models))
     
@@ -73,7 +76,31 @@ class Pipeline:
         return self.solver.train(num_epoch=num_epoch)
     
     def evaluate(self):
+        preds = []
+        targets = []
+        thresholds = np.linspace(-3, 1, num=400)
+        mcc_values = [0 for i in range(len(thresholds))]
+
+        dataloader = data.DataLoader(self.valid_set, batch_size=1, shuffle=False)
+
+        with torch.no_grad():
+            for batch in dataloader:
+                batch = utils.cuda(batch, device=torch.device(f'cuda:{self.gpus[0]}'))
+                pred, target = self.task.predict_and_target(batch)
+                preds.append(pred)
+                targets.append(target)
+
+        pred = utils.cat(preds)
+        target = utils.cat(targets)
         
+        for i, threshold in enumerate(thresholds):
+            mcc = self.task.evaluate(
+                pred, target, threshold
+            )['mcc']
+            mcc_values[i] = mcc_values[i] + mcc
+        threshold = thresholds[np.argmax(mcc_values)]
+        print(f'threshold:{threshold}\n')
+        self.task.threshold = threshold
         return self.solver.evaluate("test")
         
         
