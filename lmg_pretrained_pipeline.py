d3128b52403166baab049c05f2f9cc0bbffc3ee0
dlwocks31 <dlwocks31@gmail.com>
2023-05-25 14:09:20 +0900

    lmg_pretrained update

--- lmg_pretrained_pipeline.py
+++ lmg_pretrained_pipeline.py
@@ -4,9 +4,11 @@ from lib.disable_logger import DisableLogger
 import torch
 
 DATA_FILE_PATH = 'lmg_pretrained_pipeline.json'
-PRETRAINED_WEIGHT = 'ResidueType_lmg_6_512_0.55843.pth'
-GEARNET_HIDDEN_DIM_COUNT = 6
-GPU = 3
+PRETRAINED_WEIGHT = {
+    3: 'ResidueType_lmg_3_512_0.54631.pth',
+    6: 'ResidueType_lmg_6_512_0.55843.pth',   
+}
+GPU = 0
 
 def exp_record_exists(data, parameters):
     for record in data:
@@ -15,10 +17,11 @@ def exp_record_exists(data, parameters):
     return False
 
 
-def run_exp(data, gearnet_freeze_layer, bert_freeze_layer, trial):
+def run_exp(data, gearnet_freeze_layer, bert_freeze_layer, pretrained_layers, trial):
     parameters = {
         "gearnet_freeze_layer": gearnet_freeze_layer,
         "bert_freeze_layer": bert_freeze_layer,
+        "pretrained_layers": pretrained_layers,
         "trial": trial,
     }
     # check if the experiment has been run
@@ -34,12 +37,12 @@ def run_exp(data, gearnet_freeze_layer, bert_freeze_layer, trial):
         model_kwargs={
             'gpu': GPU,
             'gearnet_hidden_dim_size': 512,
-            'gearnet_hidden_dim_count': GEARNET_HIDDEN_DIM_COUNT,
+            'gearnet_hidden_dim_count': pretrained_layers,
             'bert_freeze': bert_freeze_layer == 30,
             'bert_freeze_layer_count': bert_freeze_layer,
         })
     
-    state_dict = torch.load(PRETRAINED_WEIGHT)
+    state_dict = torch.load(PRETRAINED_WEIGHT[pretrained_layers])
     pipeline.model.gearnet.load_state_dict(state_dict)
     pipeline.model.freeze_gearnet(freeze_layer_count=gearnet_freeze_layer)
 
@@ -55,7 +58,7 @@ def run_exp(data, gearnet_freeze_layer, bert_freeze_layer, trial):
         print(data[-1])
 
     data.sort(key=lambda x: (x["gearnet_freeze_layer"],
-              x["bert_freeze_layer"], x["trial"], x["epoch"]))
+              x["bert_freeze_layer"], x["pretrained_layers"], x["trial"], x["epoch"]))
     with open(DATA_FILE_PATH, 'w') as f:
         json.dump(data, f)
 
@@ -68,15 +71,20 @@ def main():
         # File does not exist, or it is empty
         data = []
 
-    for gearnet_freeze_layer in range(0, 6):
-        for bert_freeze_layer in range(29, 31):
-            for trial in range(1):
-                run_exp(
-                    data = data,
-                    gearnet_freeze_layer = gearnet_freeze_layer,
-                    bert_freeze_layer = bert_freeze_layer, 
-                    trial = trial
-                )
+    # add default "pretrained_layers = 6" for each entry
+    data = [{"pretrained_layers": 6} | d for d in data]
+
+    for trial in range(3):
+        for gearnet_freeze_layer in range(0, 6):
+            for bert_freeze_layer in range(29, 31):
+                for pretrained_layers in [3, 6]:
+                    run_exp(
+                        data = data,
+                        gearnet_freeze_layer=gearnet_freeze_layer,
+                        bert_freeze_layer=bert_freeze_layer, 
+                        pretrained_layers=pretrained_layers,
+                        trial = trial
+                    )
 
 
 if __name__ == '__main__':
